683f85d523e03570f204357dc20208eb8abab8bc
YiqunPeng <ypeng1@andrew.cmu.edu>
2019-04-20 19:58:32 -0700

    232

--- solutions/50_pow_x_n.py
+++ solutions/50_pow_x_n.py
@@ -1,5 +1,9 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        """Binary Search, Recursive
+
+        Running Time: O(log n)
+        """
         if n == 0:
             return 1.0
         if n == 1:
@@ -11,4 +15,23 @@ class Solution:
         
         t = self.myPow(x, n // 2)
         return t * t * self.myPow(x, n % 2)
+ 
+    def myPow_2(self, x: float, n: int) -> float:
+        """Binary Search, Iterative
+
+        Running Time: O(log n)
+        """
+        if n < 0:
+            n = -n
+            x = 1 / x
+            
+        ret = 1
+            
+        while n:
+            if n % 2:
+               ret *= x
+            x *= x
+            n //= 2
         
+        return ret 
+      
