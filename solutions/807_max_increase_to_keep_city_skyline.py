1bc23319b982baf96bd20458930c74e5f2ddf276
Yiqun Peng <ypeng0804@gmail.com>
2022-04-03 20:34:50 -0700

    Create 807_max_increase_to_keep_city_skyline.py

---
+++ solutions/807_max_increase_to_keep_city_skyline.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        row, col = [0] * n, [0] * n
+        for i in range(n):
+            for j in range(n):
+                row[i] = max(row[i], grid[i][j])
+                col[j] = max(col[j], grid[i][j])
+        res = 0
+        for i in range(n):
+            for j in range(n):
+                res += max(0, min(row[i], col[j]) - grid[i][j])
+        return res
